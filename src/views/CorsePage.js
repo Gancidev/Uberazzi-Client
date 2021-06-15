e568d947b710f7ba87898630a8ba1557be7780b5
StefoXCaffeine <stefano.corrao@yahoo.it>
2021-06-15 18:39:47 +0200

    Aggiunta la pagina CorsePage.js

--- src/views/CorsePage.js
+++ src/views/CorsePage.js
@@ -61,5 +61,58 @@ export default function CorsePage() {
         "deg)"
     );
   }
-  return("");
+  return(
+    <>
+    <PersonalNavBar />
+        <div className="wrapper">
+            <div className="page-header">
+                <div className="content">
+                <Container className="align-items-center">
+                        <Row>
+                            <Col className="offset-md-3" lg="5" md="6">
+                            </Col>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                    <th>ID</th>
+                                    <th>Partenza</th>
+                                    <th>Arrivo</th>
+                                    <th>Data e Ora</th>
+                                    <th>Accetta</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row">1</th>
+                                    <td>Palermo</td>
+                                    <td>Catania</td>
+                                    <td>07/06/2021 15:30</td>
+                                    <td><Button className="btn-simple"
+                                        color="success"><i className="tim-icons icon-check-2"/></Button></td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">2</th>
+                                    <td>VIa Ernesto Basile 15</td>
+                                    <td>Piazza Scaffa</td>
+                                    <td>07/06/2021 15:30</td>
+                                    <td><Button className="btn-simple"
+                                        color="success"><i className="tim-icons icon-check-2"/></Button></td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">3</th>
+                                    <td>Via Dei Vespri 23</td>
+                                    <td>Viale Lazio 33</td>
+                                    <td>17/03/2021 12:30</td>
+                                    <td><Button className="btn-simple"
+                                        color="success"><i className="tim-icons icon-check-2"/></Button></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Row>                                                         
+                    </Container>                    
+                </div>
+            </div>
+            <Footer />
+        </div>
+    </>);
 }
