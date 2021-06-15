9294d4cdf2d24244d9db7edc9cef9a5df830aa6d
StefoXCaffeine <stefano.corrao@yahoo.it>
2021-06-15 17:45:03 +0200

    Update VeicoliPage.js

--- src/views/VeicoliPage.js
+++ src/views/VeicoliPage.js
@@ -16,6 +16,7 @@
 
 */
 import React from "react";
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 // core components
 import PersonalNavBar from "components/Navbars/PersonalNavBar.js";
@@ -49,14 +50,93 @@ import {
   } from "reactstrap";
 
 export default function VeicoliPage() {
+    const [squares1to6, setSquares1to6] = React.useState("");
+  const [squares7and8, setSquares7and8] = React.useState("");
+  const [partenzaF, setPartenzaF] = React.useState(false);
+  const [destinazioneF, setDestinazioneF] = React.useState(false);
+  
+  React.useEffect(() => {
+    document.body.classList.toggle("register-page");
+    document.documentElement.addEventListener("mousemove", followCursor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.removeEventListener("mousemove", followCursor);
+    };
+  },[]);
+  const followCursor = (event) => {
+    let posX = event.clientX - window.innerWidth / 2;
+    let posY = event.clientY - window.innerWidth / 6;
+    setSquares1to6(
+      "perspective(500px) rotateY(" +
+        posX * 0.05 +
+        "deg) rotateX(" +
+        posY * -0.05 +
+        "deg)"
+    );
+    setSquares7and8(
+      "perspective(500px) rotateY(" +
+        posX * 0.02 +
+        "deg) rotateX(" +
+        posY * -0.02 +
+        "deg)"
+    );
+  };
     return(
       <>
         <PersonalNavBar />
         <div className="wrapper">
             <div className="page-header">
-                blabla
+                <div className="content">
+                    <Container className="align-items-center">
+                    <Row>
+                        <Col className="offset-md-3" lg="5" md="6">
+                            <div
+                                className="square square-7"
+                                id="square7"
+                                style={{ transform: squares7and8 }}
+                            />
+                            <div
+                                className="square square-8"
+                                id="square8"
+                                style={{ transform: squares7and8 }}
+                            />
+                        </Col>
+                    </Row>
+                        <Table striped variant="dark">
+                            <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Container>
+                </div>
             </div>
+            <Footer />
         </div>
-        <Footer />
       </>);
 }
