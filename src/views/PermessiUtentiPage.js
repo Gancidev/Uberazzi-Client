c6a0d4807442a54abccfd9a09324f177422a0243
Giuseppe Ganci <73232718+Gancidev@users.noreply.github.com>
2021-06-17 15:41:34 +0200

    Update PermessiUtentiPage.js
    Aggiunte icone in movimento al posto dei quadrati

--- src/views/PermessiUtentiPage.js
+++ src/views/PermessiUtentiPage.js
@@ -27,6 +27,7 @@ import {
   var id_utente=0;
 
 export default function PermessiUtenti() {
+    const [squares1to6, setSquares1to6] = React.useState("");
   const [squares7and8, setSquares7and8] = React.useState("");
   const [formModal, setFormModal] = React.useState(false);
   function apriForm(id){
@@ -52,6 +53,13 @@ export default function PermessiUtenti() {
   const followCursor = (event) => {
     let posX = event.clientX - window.innerWidth / 2;
     let posY = event.clientY - window.innerWidth / 6;
+    setSquares1to6(
+        "perspective(500px) rotateY(" +
+          posX * 0.05 +
+          "deg) rotateX(" +
+          posY * -0.05 +
+          "deg)"
+      );
     setSquares7and8(
       "perspective(500px) rotateY(" +
         posX * 0.02 +
@@ -69,18 +77,42 @@ export default function PermessiUtenti() {
                     <Container className="align-items-center">
                     <Row>
                         <Col className="offset-md-3" lg="5" md="6">
-                            <div
-                                className="square square-7"
-                                id="square7"
-                                style={{ transform: squares7and8 }}
-                            />
-                            <div
-                                className="square square-8"
-                                id="square8"
-                                style={{ transform: squares7and8 }}
-                            />
+                            <img
+                                    alt="..."
+                                    className="square square-7_personale"
+                                    id="square7"
+                                    src={require("assets/veicoli/auto1.png").default}
+                                    style={{ transform: squares7and8 }}
+                                />
+                                <img
+                                    alt="..."
+                                    className="square square-8_personale"
+                                    id="square8"
+                                    src={require("assets/veicoli/auto2.png").default}
+                                    style={{ transform: squares7and8 }}
+                                />
+                                <img
+                                    alt="..."
+                                    className="square square-4_personale"
+                                    id="square4"
+                                    src={require("assets/veicoli/moto1.png").default}
+                                    style={{ transform: squares1to6 }}
+                                />
+                                <img
+                                    alt="..."
+                                    className="square square-5_personale"
+                                    id="square5"
+                                    src={require("assets/veicoli/moto3.png").default}
+                                    style={{ transform: squares1to6 }}
+                                />
+                                <img
+                                    alt="..."
+                                    className="square square-6_personale"
+                                    id="square6"
+                                    src={require("assets/veicoli/monopattino1.png").default}
+                                    style={{ transform: squares1to6 }}
+                                />
                         </Col>
-                    </Row>
                         <Table striped variant="dark">
                             <thead>
                                 <tr>
@@ -139,6 +171,14 @@ export default function PermessiUtenti() {
                                 </tr>
                             </tbody>
                         </Table>
+                        </Row>
+                        <img
+                            alt="..."
+                            className="square square-3_personale"
+                            id="square3"
+                            src={require("assets/veicoli/bici1.png").default}
+                            style={{ transform: squares1to6 }}
+                        /> 
                         {/* Start Form Modal */}
                         <Modal
                             modalClassName="modal-black"
